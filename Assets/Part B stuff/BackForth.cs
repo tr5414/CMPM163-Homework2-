73ed7ca72ade5fc0b73b9c1017ab7a2c11fc86ac
Thomas Ruiz <lovecraft5414@gmail.com>
2019-05-18 23:27:25 -0700

    Assighnment 2

---
+++ Assets/Part B stuff/BackForth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackForth : MonoBehaviour
+{
+    private Vector3 pos1 = new Vector3(-5, 0, 35);
+    private Vector3 pos2 = new Vector3(5, 0, 35);
+    public float speed = 0.5f;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+    }
+}
---
+++ Assets/Part B stuff/Materials/Example.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    public Cubemap texture;
+
+    void Start()
+    {
+        // Create a new Cubemap texture.
+        texture = new Cubemap(128, TextureFormat.RGBA32, false);
+    }
+}
